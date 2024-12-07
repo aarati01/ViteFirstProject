75e2253c7b9f940643b2d8a35d556ea13756c04b
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-12-07 02:33:08 -0500

    image added for all the div

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 const Card = (props) => {
     return (
       <div className="Card">
+        <img src="https://images.pexels.com/photos/28290901/pexels-photo-28290901/free-photo-of-buho-de-anteojos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 " width={242} height={150}/>
         <h1>{props.title}</h1>
         <p>{props.description}</p>
       </div>
