d36b9febeaf679b5a411419f4ecef9a16f416e08
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-12-07 11:37:49 -0500

    useState is add

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react"
 import "../components/Card.css"
 import PropTypes from "prop-types"
 const Card = (props) => {
+  const [count,setcount]=useState(100)
     return (
-      <div className="Card">
-        <img src="https://images.pexels.com/photos/28290901/pexels-photo-28290901/free-photo-of-buho-de-anteojos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 " width={242} height={150}/>
+      <div className="Card" style={{overflow:"hidden"}}>
+        <img src="https://images.pexels.com/photos/28290901/pexels-photo-28290901/free-photo-of-buho-de-anteojos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 " width={242} height={150}
+        style={{border:"2px solid black"}}/>
         <h1>{props.title}</h1>
         <p>{props.description}</p>
+
+        <div>The count is {count}</div>
+        <button onClick={()=>{setcount(count+1)}}>UPdate Count</button>
       </div>
   )
 }
